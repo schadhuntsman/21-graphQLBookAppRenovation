61510ffd172e89e42db27ca54992f9abd0dfdca9
schadhuntsman <104479454+schadhuntsman@users.noreply.github.com>
2022-09-21 16:11:34 -0700

    updated resolvers and typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,10 +1,13 @@
 // import the gql tagged template function
+const { Book, User } = require('../models');
 const { gql } = require('apollo-server-express');
 
 const resolvers = {
       Query: {
             books: async () => {
-                  return Books.find().sort({ createdAt: -1 });
+                  return Book.find().sort({ createdAt: -1 });
             }
       }
 };
+
+module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,22 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
+// const { Book, User } = require('../models');
+// const { bookSchema } = require('../models/Book')
+// const { bookSchema } = {Book}
 
-type Query {
-      books: [Book]
+// type Query {
+
+//       books(authors: String): [Book]
       
-    }
+//     }
+const typeDefs = gql`
+type User {
+      _id: ID
+      username: String
+      email: String
+      savedBooks: [User]
+      bookCount: [User]
+}
 
 type Book {
       _id: ID
@@ -15,3 +27,5 @@ type Book {
       link: String
       title: Strring
 }
+`
+module.exports = typeDefs;
