9c3b423c35b1d60b5209df9cf8169efbc7caa524
schad huntsman <schadhuntsman@gmail.com>
2022-09-24 14:01:19 -0700

    a

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,7 +4,12 @@ const jwt = require('jsonwebtoken');
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
-module.exports = {
+// module.exports = {
+//   signToken: funtion({ username, email, _id}) {
+//     const payload = {username, email, _id}
+//   }
+
+//   return jwt.sign({ data: payload }, secret, { expiresIn: expression });
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
@@ -30,10 +35,13 @@ module.exports = {
 
     // send to next endpoint
     next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
+  }
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+
+ module.exports = {
+  signToken: funtion({ username, email, _id}) {
+    const payload = {username, email, _id}
+  }
+
+  return jwt.sign({ data: payload }, secret, { expiresIn: expression });
 };
